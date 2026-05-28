8ba16b3ffcda3ed8f25aa26a1a70f34e07625fdb
Bryandc <bryandc@bizmoto.com.ph>
2026-05-28 23:24:28 +0800

    Add S3 backend for persistent state storage

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ terraform {
       version = ">= 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "terraform-cicd-bucket-git"
+    key            = "s3-state/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-eks-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
