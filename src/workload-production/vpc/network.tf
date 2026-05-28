954e81d57be33f373b9a582fda5548db93018d5c
Bryandc <bryandc@bizmoto.com.ph>
2026-05-28 23:45:38 +0800

    Restructure project and fix paths

---
+++ src/workload-production/vpc/network.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  source   = "../../../modules/vpc"
+  vpc_name = "eks-vpc"
+
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones   = ["ap-southeast-1a", "ap-southeast-1b"]
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
