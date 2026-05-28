ea5c975cbc1195159bf96164ff48ff96fc58032b
Bryandc <bryandc@bizmoto.com.ph>
2026-05-29 02:10:18 +0800

    Add force_delete to ECR for destroy

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "this" {
   name                 = var.name
   image_tag_mutability = var.image_tag_mutability
+  force_delete         = true
 
   encryption_configuration {
     encryption_type = var.encryption_type
