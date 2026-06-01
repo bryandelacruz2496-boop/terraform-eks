14aa462910a49145da6db321c620bc0d8a1cb2d1
Bryandc <bryandc@bizmoto.com.ph>
2026-06-02 00:09:15 +0800

    Create eks

--- src/workload-production/eks.tf
+++ src/workload-production/eks.tf
@@ -1,33 +1,33 @@
-# module "eks" {
-#   source          = "../../modules/eks"
-#   cluster_name    = "eks-cluster-new"
-#   cluster_version = "1.30"
-#   region          = "ap-southeast-1"
+module "eks" {
+  source          = "../../modules/eks"
+  cluster_name    = "eks-cluster-new-demo"
+  cluster_version = "1.30"
+  region          = "ap-southeast-1"
 
-#   subnet_ids     = module.vpc.private_subnet_ids
-#   instance_types = ["t3.micro"]
-#   desired_size   = 2
-#   min_size       = 1
-#   max_size       = 3
+  subnet_ids     = module.vpc.private_subnet_ids
+  instance_types = ["t3.micro"]
+  desired_size   = 2
+  min_size       = 1
+  max_size       = 3
 
-#   tags = {
-#     Name = "eks-cluster"
-#   }
-# }
+  tags = {
+    Name = "eks-cluster-test"
+  }
+}
 
-# # Access entry for eks-test user (kubectl/developer access)
-# resource "aws_eks_access_entry" "eks_test_user" {
-#   cluster_name  = module.eks.cluster_name
-#   principal_arn = "arn:aws:iam::767397778742:user/eks-test"
-#   type          = "STANDARD"
-# }
+# Access entry for eks-test user (kubectl/developer access)
+resource "aws_eks_access_entry" "eks_test_user" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = "arn:aws:iam::767397778742:user/eks-test"
+  type          = "STANDARD"
+}
 
-# resource "aws_eks_access_policy_association" "eks_test_admin" {
-#   cluster_name  = module.eks.cluster_name
-#   principal_arn = "arn:aws:iam::767397778742:user/eks-test"
-#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+resource "aws_eks_access_policy_association" "eks_test_admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = "arn:aws:iam::767397778742:user/eks-test"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
-#   access_scope {
-#     type = "cluster"
-#   }
-# }
+  access_scope {
+    type = "cluster"
+  }
+}
